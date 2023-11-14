1cd265886a5d0a315b29d35e1f2f59f7c08b8e2b
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-11-14 10:14:59 +0200

    sass: up and running

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,3 +1,9 @@
+import variables from "@/style/variables.module.scss";
+
 export default function Home() {
-  return <p className="justText">Hey</p>;
+  return (
+    <p className="justText" style={{ color: variables.primaryColor }}>
+      Hey
+    </p>
+  );
 }
